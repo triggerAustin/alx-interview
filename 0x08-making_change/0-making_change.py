6e04c1aaa2607babe84b5185c93beca04e412680
austin <trigger_austin@proton.me>
2024-12-24 15:02:52 +0300

    last minute

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Makeing change module
+"""
+
+
+def makeChange(coins, total):
+    """
+    Calculates the minimum number of coins required to make a given total
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
